3b0ec450d07f79dac2ec801f259e9e353e2c6017
ash-mcc <ashley_mcclenaghan@mac.com>
2023-02-20 15:28:22 +0000

    adding the DVC stuff

---
+++ src/clj/deceptively_simple.clj
@@ -0,0 +1,68 @@
+(ns deceptively-simple)
+
+
+
+// Preamble
+//
+
+
+// me: backgorund: dist sys arch > RPC, CORBA > Oak, Java > banks ... > data sci-y
+// interests: applied xyz, enterprise tech/interop
+
+
+// these days... almost pick your platform and lang 
+// me: plat=JVM, lang=...
+// want: it simple! small syntax, ~0 boilerplate, good interop... >>> Clojure
+// demo some...
+
+
+
+// Deceptively simple
+//
+
+
+// this env: a REPL ...REPL server ...jack-in ...interpretes Clojure 'forms'
+
+
+// simple value, list (...LISP), homoiconic, vars,  nice literals
+
+1
+
+(+ 1 2)
+
+'(+ 1 2)
+
+(def xs [1 2 3 4 5])
+
+(def m {:a 1
+        :b 2})
+
+
+// a lib of useful, composable functions over a very small number of composable data types
+
+(get xs 0)
+
+(get m :a)
+
+
+// proper value equality
+
+(= #{1 2 [3 4]} 
+   #{2 [3 4] 1})
+
+
+// immutable data, data sharing impl
+// ++ for concurrency - easy to reason about state, not chopped up and wrapped in code
+
+(def ys (conj xs 0))
+
+
+// values vs stateful entities/identities ...'a stable logical entity associated with a series of different values over time'
+
+(def x-through-time (atom {:a "initial state"}))
+
+(deref x-through-time)
+
+(swap! x-through-time assoc :a "state 3")
+
+@x-through-time
---
+++ src/clj/plays_nicely_with_others.clj
@@ -0,0 +1,71 @@
+(ns plays-nicely-with-others)
+
+
+;; bridging to/from R
+;;
+
+;; load the Clojure-R bridge library
+(require '[clojisr.v1.r :as r :refer [require-r]]) 
+            
+;; ask R to interprete a string as code
+;; and assign it to a Clojure var
+(def x (r/r "1 + 5"))
+
+;; an RObject is a Clojure reference to an R variable (value or function)
+(type x)
+
+;; convert Clojure form into R code
+(r/clj->r [1 nil 3])
+
+;; R code as a string
+(def f (r/r "function(x) x*10")) 
+
+;; Clojure -> R -> Clojure
+(-> 5 
+    f 
+    r/r->clj)
+
+;; load R's datasets library
+(require-r '[datasets :as datasets]) 
+
+;; get the Iris dataset
+(def iris-df datasets/iris)
+(def class (r/r "class"))
+(class iris-df) ;; class in R
+(type iris-df) ;; type in Clojure
+
+;; call an R function on it
+((r/r "summary") iris-df)
+
+;; element getters/setters
+(r/bra iris-df 1 "Sepal.Length") ;; df[1,"Sepal.Length"]
+(r/bra<- iris-df 1 "Sepal.Length" 1.2345) ;; df[1,"Sepal.Length"]<-4.2
+
+;; load a couple more libs
+(require-r '[GGally :as GGally]) ;; ggplot2 specialised 
+(require '[clojisr.v1.applications.plotting :as plotting]) ;; Clojure-R bridge helper
+
+;; pairwise comparison of the variables 
+(->> iris-df
+     GGally/ggpairs
+     (plotting/plot->file "iris.svg"))
+
+;; zero-copy data sharing between Clojure and R
+(def iris-ds (r/r->clj iris-df))
+(type iris-ds)
+
+;; load Clojure dataset functions 
+(require '[scicloj.ml.dataset :as ds])
+
+;; a bit like R's summary() function
+(ds/info iris-ds)
+
+
+
+;; hosted on the JVM
+;;
+
+(System/getProperty "java.version") ;; Java version
+
+(import )
+
